62cff778360c819f4c9d65bed6229b97e2cecad3
youldaw <68648932+youldaw@users.noreply.github.com>
2023-01-03 05:48:41 +0500

    ready
    ready all

--- js/script.js
+++ js/script.js
@@ -3,9 +3,88 @@ $(function (){
     $('input[name=phone]').mask('+7 (999) 999-99-99');
 
 
-    $('.menu-mob, .menu-closer').on('click',function(e){
+    $('.menu-opener, .menu-closer').on('click',function(e){
         e.preventDefault();
-        $('.top-left').toggleClass('active');
+        $('header .navbar').toggleClass('active');
+        $('header').toggleClass('active');
+    });
+
+    $('.more-video').on('click',function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $('.comment-video-col.hide').slideToggle("slow");
+    });
+
+    $('.card-link a').on('click',function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $('.card-text .hide').slideToggle("slow");
+    });
+
+    $('.place-slide').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        dots:false,
+        // center: false,
+        smartSpeed:900,
+        navText: ['<img src="images/slide-arrow-left.png">','<img src="images/slide-arrow-right.png">'],
+        items:1,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    });
+
+    $('.comment-more-slide').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        dots:false,
+        // center: false,
+        smartSpeed:900,
+        navText: ['<img src="images/slide-arrow-left.png">','<img src="images/slide-arrow-right.png">'],
+        items:1,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    });
+
+    $('.live-slide').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        dots:false,
+        // center: false,
+        smartSpeed:900,
+        navText: ['<img src="images/slide-arrow-left.png">','<img src="images/slide-arrow-right.png">'],
+        items:1,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
     });
 
 });
